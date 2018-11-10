130cf01212a4aeebfc6804061b79f82c899695f5
Deviprasad, Divyaranjan (US - Bengaluru) <ddeviprasad@deloitte.com>
2018-11-10 17:58:02 +0530

    hello world api

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello Google Home');
+});
 
 // starting a server
 app.listen(PORT, () => {
