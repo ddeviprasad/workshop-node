cdd7816b18d1d7a7be34f1ca8717db9a9b593dd9
Deviprasad, Divyaranjan (US - Bengaluru) <ddeviprasad@deloitte.com>
2018-11-11 22:00:56 +0530

    dummy-db

--- db/mangoose.js
+++ db/mangoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Movie');
+// mongoose.connect('mongodb://localhost/Movie');
+mongoose.connect('mongodb://admin:admin123@ds241012.mlab.com:41012/movie');
 
 module.exports = { mongoose };
